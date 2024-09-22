a9e79e592574fa97890a48e91001508b25247136
Yain aitHamid <yassinaithamid@gmail.com>
2024-09-22 21:27:03 +0100

    origins update *

--- src/cors.js
+++ src/cors.js
@@ -3,9 +3,6 @@ import FastifyCors from "@fastify/cors";
 export const setFastifyCors = function (fastify) {
   fastify.register(FastifyCors, {
     // put your options here
-    origin: [
-      "https://66f0745a6eb9530008388b5a--illustrious-dasik-79c411.netlify.app/",
-      
-    ],
+    origin: '*',
   });
 };
