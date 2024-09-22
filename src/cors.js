7b66aa8b574cddad757ba5f617c7f303f8a54487
Yain aitHamid <yassinaithamid@gmail.com>
2024-09-22 17:08:52 +0100

    Update .gitignore to not  ignore .env and modify settings

--- src/cors.js
+++ src/cors.js
@@ -4,9 +4,9 @@ export const setFastifyCors = function (fastify) {
   fastify.register(FastifyCors, {
     // put your options here
     origin: [
-      "http://0.0.0.0:9000",
-      "http://0.0.0.0:8100",
-      "http://0.0.0.0:4200",
+      "http://localhost:9000",
+      "http://localhost:8100",
+      "http://localhost:4200",
     ],
   });
 };
